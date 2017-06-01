d4e34fc78a60941d0799c6c6afbebc92a17690eb
Manish Rathi <manishrathi19902013@gmail.com>
2017-06-01 12:38:56 +0530

    Code Clean-up.

--- MRCoreDataManager.podspec
+++ MRCoreDataManager.podspec
@@ -2,7 +2,7 @@ Pod::Spec.new do |s|
   s.name         = "MRCoreDataManager"
   s.version      = "1.0.0"
   s.summary      = "A framework to easily perform coredata operations. i.e Add, Fetch, Update, Delete."
-  s.platform     = :ios, "9.0"
+  s.platform     = :ios, "10.0"
   s.author       = 'Manish Rathi - manishrathi19902013@gmail.com'
   s.license	     = { :type => 'Manish', :text => <<-LICENSE
                         © Manish Rathi, 2017. All rights reserved.
--- Source/MRCoreDataManager/MRCoreDataManager/MRCoreDataManager.swift
+++ Source/MRCoreDataManager/MRCoreDataManager/MRCoreDataManager.swift
@@ -25,7 +25,32 @@
 // THE SOFTWARE.
 
 import Foundation
+import CoreData
 
 open class MRCoreDataManager {
+    let persistentContainerName = "MRCoreDataManagerDemo"
     
+    // MARK: - Core Data stack
+    var persistentContainer: NSPersistentContainer {
+        let container = NSPersistentContainer(name: persistentContainerName)
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
